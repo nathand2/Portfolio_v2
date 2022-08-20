439072c734c91e808e0e65748f76b03ea4521f15
Nathan <ndong5@my.bcit.ca>
2022-08-20 16:35:08 -0700

    landing page img changes

--- components/Landing.js
+++ components/Landing.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/Landing.module.css";
 
+import landingImg from '../public/content-bg.jpg'
+import chappieImg from '../public/chappie.jpg';  // 🐶
+
 const Landing = () => {
   return (
     <div className={styles.container}>
-      <Image src="/content-bg.jpg" className={styles.graphic} alt="vancouver, night" layout='fill' />
-      {/* <img src="/content-bg.jpg" alt="vancouver" className={styles.graphic} /> */}
+      <Image src={landingImg} className={styles.graphic} alt="vancouver, night" layout='fill' placeholder='blur' />
       <div className={styles.overlay}>
         <div className={styles.content_container}>
           <h1 className={`${styles.left}`}>Hey</h1>
@@ -17,13 +19,8 @@ const Landing = () => {
           <h6>Actual Human and Software Developer</h6>
           <div className={styles.chappie_container}>
             <div className={styles.chappie_img_container}>
-              <Image src="/chappie.jpg" alt="dog, chappie" className={styles.chap}  layout='fill'/>
+              <Image src={chappieImg} alt="dog, chappie" className={styles.chap} layout='fill' placeholder='blur' />
             </div>
-            {/* <img
-              src="/chappie.jpg"
-              alt="dog, chappie"
-              className={styles.chap}
-            /> */}
             <h6 className={styles.text_small}>
               &quot;Good developer, better belly rubber. 8/10&quot; - Chappie 2022
             </h6>
