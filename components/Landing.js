b971bac6b83e32e406aed8c8bdd91ff8f89a3de2
Nathan <ndong5@my.bcit.ca>
2022-08-24 14:26:04 -0700

    landing removed blur

--- components/Landing.js
+++ components/Landing.js
@@ -7,7 +7,7 @@ import chappieImg from '../public/chappie.jpg';  // 🐶
 const Landing = () => {
   return (
     <div className={styles.container}>
-      <Image src={landingImg} className={styles.graphic} alt="vancouver, night" layout='fill' placeholder='blur' />
+      <Image src={landingImg} className={styles.graphic} alt="vancouver, night" layout='fill' />
       <div className={styles.overlay}>
         <div className={styles.content_container}>
           <h1 className={`${styles.left}`}>Hey</h1>
@@ -19,17 +19,13 @@ const Landing = () => {
           <h6>Actual Human and Software Developer</h6>
           <div className={styles.chappie_container}>
             <div className={styles.chappie_img_container}>
-              <Image src={chappieImg} alt="dog, chappie" className={styles.chap} layout='fill' placeholder='blur' />
+              <Image src={chappieImg} alt="dog, chappie" className={styles.chap} layout='fill' />
             </div>
             <h6 className={styles.text_small}>
               &quot;Good developer, better belly rubber. 8/10&quot; - Chappie 2022
             </h6>
           </div>
           <br />
-          {/* <h6 className={styles.text_small}>
-            Click to tell Nathan to{" "}
-            <span className={styles.accent_blue}>Give Me A Treat</span>
-          </h6> */}
         </div>
       </div>
     </div>
