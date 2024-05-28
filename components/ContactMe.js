ed3668b32f7e4ee80e97210c78c1dc47321d520e
Nathan Dong <ndong5@my.bcit.ca>
2024-05-27 17:41:24 -0700

    change

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Image from "next/image";
 import styles from "../styles/ContactMe.module.css";
 
 const ContactMe = () => {
   return (
     <div className={styles.container}>
       <p className={styles.text}>Get in touch!</p>
-      <img src="arrow.svg" className={styles.arrow} alt="contact me!!! pretty please"/>
+      <Image src="arrow.svg" className={styles.arrow} alt="contact me!!! pretty please" width={100} height={100}/>
     </div>
   )
 }
